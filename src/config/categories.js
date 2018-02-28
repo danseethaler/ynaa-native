f7eab61ad211b0c2ee242674e55b39c0ad0de485
Dan Seethaler <dan@seethalers.com>
2018-02-28 14:38:48 -0500

    Config and mock data

---
+++ src/config/categories.js
@@ -0,0 +1,92 @@
+export default [
+  {
+    key: 'income',
+    title: 'Income (typically after-tax)',
+    includes: [
+      // * Paycheck
+      // * Side jobs
+    ],
+  },
+  {
+    key: 'savings',
+    title: 'Savings',
+    includes: [
+      // * Monthly Savings amount (recommended 10% of earnings)
+      // * Retirement (401k, IRA)
+    ],
+  },
+  {
+    key: 'shelter',
+    title: 'Shelter',
+    includes: [
+      // * Mortgage/Rent
+      // * HOA
+    ],
+  },
+  {
+    key: 'utilities',
+    title: 'Utilities',
+    includes: [
+      // * Natural Gas
+      // * Electricity
+      // * Cell phone bill
+      // * Internet
+      // * Water
+      // * Garbage, city dues, etc.
+    ],
+  },
+  {
+    key: 'transportation',
+    title: 'Transportation',
+    includes: [
+      // * Car Payment, city bus pass
+      // * Auto insurance
+      // * Maintenance
+      // * Yearly vehicle registration
+    ],
+  },
+  {
+    key: 'medical',
+    title: 'Medical',
+    includes: [
+      // * Health Insurance Premiums
+      // * Monthly additional amount (think co-pays, checkups, emergencies)
+      // * Dental
+    ],
+  },
+  {
+    key: 'subscriptions',
+    title: 'Subscriptions',
+    includes: [
+      // * Music
+      // * Cloud storage (DropBox, Google Drive)
+      // * Gym memberships
+      // * Annual dues
+    ],
+  },
+  {
+    key: 'education',
+    title: 'Education',
+    includes: [
+      // * Tuition
+      // * Kid’s school costs
+    ],
+  },
+  {
+    key: 'loan',
+    title: 'Loan Payments',
+    includes: [
+      // * Student loans
+      // * Personal loans
+      // * Credit card payoff
+    ],
+  },
+  {
+    key: 'donations',
+    title: 'Donations (bring good karma into your life)',
+    includes: [
+      // * Monthly charitable contribution
+      // * Tithing / fast offering
+    ],
+  },
+];
